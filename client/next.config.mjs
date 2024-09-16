885f372e459c3054995f01dff2b6e96c08eaf769
Joan <joan.diazestigarribia@innew.la>
2024-09-16 12:15:50 -0300

    config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+    // other exports
+    target: 'serverless', // add this line
+  };
 
 export default nextConfig;
